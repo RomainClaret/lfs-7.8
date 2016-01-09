893e975c611072cf1ce862cc0684fabe6a962932
Romain Claret <contact@rocla.ch>
2016-01-09 18:10:20 +0100

    / test fix for set error

--- script-root_library-check.sh
+++ script-root_library-check.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+set lib
 for lib in lib{gmp,mpfr,mpc}.la; do
   echo $lib: $(if find /usr/lib* -name $lib|
-                  grep -q $lib;then :;else echo not;fi) found
+               grep -q $lib;then :;else echo not;fi) found
 done
 unset lib
