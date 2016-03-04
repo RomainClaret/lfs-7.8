418f588cbf312d84d48b088e4c06c8be6daaa676
Romain Claret <contact@rocla.ch>
2016-03-04 22:39:16 +0100

    + 6.all part 1 + 6.all part 2 skeleton + lfs 7.8 book

---
+++ 6.all-part-1-root_installing-basic-system.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+echo ""
+echo "### -------------------------------"
+echo "###    INSTALLING BASIC SYSTEM  ###"
+echo "###         CHAPTER 6.all       ###"
+echo "### Installing Basic System Software"
+echo "### Must be run as \"lfs\" user"
+echo "### -------------------------------"
+
+echo ""
+echo "... Loading commun functions and variables"
+if [ ! -f ./script-all_commun-functions.sh ]
+then
+  echo "!! Fatal Error 1: './script-all_commun-functions.sh' not found."
+  exit 1
+fi
+source ./script-all_commun-functions.sh
+
+if [ ! -f ./script-all_commun-variables.sh ]
+then
+  echo "!! Fatal Error 1: './script-all_commun-variables.sh' not found."
+  exit 1
+fi
+source ./script-all_commun-variables.sh
+
+echo ""
+echo "... Validating the environment"
+check_partitions
+is_user lfs
+
+./6.2-root_preparing-virtual-kernel.sh
+./6.3-root_mounting-virtual-kernel.sh
+./6.4-root_chroot-environment.sh
---
+++ 6.all-part-2-chroot_installing-basic-system.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+echo ""
+echo "### -------------------------------"
+echo "###    INSTALLING BASIC SYSTEM  ###"
+echo "###         CHAPTER 6.all       ###"
+echo "### Installing Basic System Software"
+echo "### Must be run as \"lfs\" user"
+echo "### -------------------------------"
+
+echo ""
+echo "... Loading commun functions and variables"
+if [ ! -f ./script-all_commun-functions.sh ]
+then
+  echo "!! Fatal Error 1: './script-all_commun-functions.sh' not found."
+  exit 1
+fi
+source ./script-all_commun-functions.sh
+
+if [ ! -f ./script-all_commun-variables.sh ]
+then
+  echo "!! Fatal Error 1: './script-all_commun-variables.sh' not found."
+  exit 1
+fi
+source ./script-all_commun-variables.sh
+
+echo ""
+echo "... Validating the environment"
+check_partitions
+is_user lfs
+
+./6.5-chroot_creating-directories.sh
