5026e09c3d07570be8bb3fd280ef15ab6ebbc772
Romain Claret <contact@rocla.ch>
2016-03-09 19:33:11 +0100

    / tested and fixed up to 6.9 + 6.32 ... 6.34

---
+++ 6.34-chroot_grep.sh
@@ -0,0 +1,79 @@
+#!/tools/bin/bash
+
+CHAPTER_SECTION=34
+INSTALL_NAME=grep
+
+echo ""
+echo "### ---------------------------"
+echo "###             GREP        ###"
+echo "###        CHAPTER 6.$CHAPTER_SECTION      ###"
+echo "### Grep-2.21"
+echo "### Must be run as \"chroot\" user"
+echo "### ---------------------------"
+
+echo ""
+echo "... Loading commun functions and variables"
+if [ ! -f ./script-all_commun-functions.sh ]
+then
+  echo "!! Fatal Error 1: './script-all_commun-functions.sh' not found."
+  exit 1
+fi
+source ./script-all_commun-functions.sh
+
+if [ ! -f ./script-all_commun-variables.sh ]
+then
+  echo "!! Fatal Error 1: './script-all_commun-variables.sh' not found."
+  exit 1
+fi
+source ./script-all_commun-variables.sh
+
+echo ""
+echo "... Validating the environment"
+is_user root
+check_chroot
+
+echo ""
+echo "... Setup building environment"
+LOG_FILE=$LFS_BUILD_LOGS_6$CHAPTER_SECTION-$INSTALL_NAME
+cd /sources
+check_tarball_uniqueness
+extract_tarball
+cd $(ls -d /sources/$INSTALL_NAME*/)
+
+echo ""
+echo "... Installation starts now"
+time {
+
+  echo ".... Pre-Configuring $SOURCE_FILE_NAME"
+  sed -i -e '/tp++/a  if (ep <= tp) break;' src/kwset.c
+
+  echo ".... Configuring $SOURCE_FILE_NAME"
+  ./configure     \
+    --prefix=/usr \
+    --bindir=/bin \
+	  &> $LOG_FILE-configure.log
+
+	echo ".... Making $SOURCE_FILE_NAME"
+  make $PROCESSOR_CORES &> $LOG_FILE-make.log
+
+  echo ".... Make Checking $SOURCE_FILE_NAME"
+  make check $PROCESSOR_CORES &> $LOG_FILE-make-check.log
+
+	echo ".... Installing $SOURCE_FILE_NAME"
+  make install $PROCESSOR_CORES &> $LOG_FILE-make-install.log
+
+}
+
+echo ""
+echo "... Cleaning up $SOURCE_FILE_NAME"
+cd /sources
+[ ! $SHOULD_NOT_CLEAN ] && rm -rf $(ls -d /sources/$INSTALL_NAME*/)
+
+echo ""
+echo "######### END OF CHAPTER 6.$CHAPTER_SECTION ########"
+echo "///// HUMAN REQUIRED \\\\\\\\\\\\\\\\\\\\"
+echo "### Please run the next step:"
+echo "### ./6.35-chroot_readline.sh"
+echo ""
+
+exit 0
