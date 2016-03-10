a0047b531412c8cfd2a36f23ee26cff99178d50f
Romain Claret <contact@rocla.ch>
2016-03-10 22:26:31 +0100

    / checked up 6.47 / typos

--- script-all_commun-functions.sh
+++ script-all_commun-functions.sh
@@ -242,7 +242,7 @@ function get_build_errors_5
 		echo "!! Info: Known errors and not critical:"
 		echo "Chapters 5.5, 5.7, 5.12, 5.14, 5.15, 5.16, 5.30"
 		echo ""
-		echo "!! Info: Until now you had $ERRORS_COUNTER errors, however they are not all critical. Crtical errors are displayed below."
+		echo "!! Info: Until now you had $ERRORS_COUNTER errors, however they are not all critical. Crtical errors are displayed below:"
     grep -n " [Ee]rrors*:* \|^FAIL:" $LFS_BUILD_LOGS_5* | grep -v "_5_5\|_5_7\|_5_12\|_5_14\|_5_15\|_5_16\|_6_30"
     echo "--> If any error, please check on http://www.linuxfromscratch.org/lfs/build-logs for comparaison."
 	else
@@ -259,7 +259,7 @@ function get_build_errors_mnt_lfs
 	ERRORS_COUNTER=$(grep -n " [Ee]rrors*:* \|^FAIL:" $LFS_BUILD_LOGS_MNT* | wc -l)
 
 	if [ $ERRORS_COUNTER -ne 0 ]; then
-		echo "!! Info: Until now you had $ERRORS_COUNTER errors, however they are not all critical. Crtical errors are displayed below."
+		echo "!! Info: Until now you had $ERRORS_COUNTER errors, however they are not all critical. Crtical errors are displayed below:"
     grep -n " [Ee]rrors*:* \|^FAIL:" $LFS_BUILD_LOGS_MNT* | grep -v ""
     echo "--> If any error, please check on http://www.linuxfromscratch.org/lfs/build-logs for comparaison."
 	else
@@ -279,7 +279,7 @@ function get_build_errors_6
 		echo "!! Info: Known errors and not critical:"
 		echo "Chapters 6.9, 6.17, 6.28, 6.31, 6.33, 6.34, 6.36, 6.38, 6.40, 6.44, 6.45, 6.46"
 		echo ""
-		echo "!! Info: Until now you had $ERRORS_COUNTER errors, however they are not all critical. Crtical errors are displayed below."
+		echo "!! Info: Until now you had $ERRORS_COUNTER errors, however they are not all critical. Crtical errors are displayed below:"
     grep -n " [Ee]rrors*:* \|^FAIL:" $LFS_BUILD_LOGS_6* | grep -v "_6_9\|_6_17\|_6_28\|_6_31\|_6_33\|_6_34\|_6_36\|_6_38\|_6_40\|_6_44\|_6_45\|_6_46"
     echo "--> If any error, please check on http://www.linuxfromscratch.org/lfs/build-logs for comparaison."
 	else
@@ -302,7 +302,7 @@ function get_build_errors_7
 		echo "!! Info: Known errors and not critical:"
 		echo "Chapters 7.X"
 		echo ""
-		echo "!! Info: Until now you had $ERRORS_COUNTER errors, however they are not all critical. Crtical errors are displayed below."
+		echo "!! Info: Until now you had $ERRORS_COUNTER errors, however they are not all critical. Crtical errors are displayed below:"
 		grep -n " [Ee]rrors*:* \|^FAIL:" $LFS_BUILD_LOGS_7* | grep -v ""
 		echo "--> If any error, please check on http://www.linuxfromscratch.org/lfs/build-logs for comparaison."
 	else
